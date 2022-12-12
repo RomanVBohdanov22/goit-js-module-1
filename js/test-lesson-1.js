1aa8e25e01248e504f69b9c7ad330c9c86af5b0b
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-12 18:06:02 +0200

    tests...

--- js/test-lesson-1.js
+++ js/test-lesson-1.js
@@ -8,3 +8,18 @@ const isFemale = true;
 const isHumam = false;
 
 console.log(userName, isFemale, isHumam);
+
+const type = typeof userName;
+console.log('typeof userName', type);
+let type2 = typeof isHumam;
+console.log('typeof isHumam', type2);
+type2 = typeof b;
+console.log('typeof:: type2 = typeof b ', type2);
+
+/*
+console.log('Перед');
+alert("Вікно попередження б'є");
+console.log('Після');
+
+type2 = typeof "Вікно попередження б'є";
+console.log("Вікно попередження б'є", type2);*/
