f523b2510cba484d8c4b2d3b8dd37b21b75cba86
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-12 12:24:16 +0200

    js started

---
+++ js/test-lesson-1.js
@@ -0,0 +1,10 @@
+const a = 200.1;
+let b = 5.0;
+console.log("a=", a);
+console.log("b=", b);
+
+const userName = 'Erika';
+const isFemale = true;
+const isHumam = false;
+
+console.log(userName, isFemale, isHumam);
