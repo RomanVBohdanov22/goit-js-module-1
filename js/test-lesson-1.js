4793214ab1cb056cfd30ebf440c2fbd77709fbe6
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-12 20:14:30 +0200

    trainings

--- js/test-lesson-1.js
+++ js/test-lesson-1.js
@@ -23,3 +23,59 @@ console.log('Після');
 
 type2 = typeof "Вікно попередження б'є";
 console.log("Вікно попередження б'є", type2);*/
+
+/*const shouldRenew = confirm("Are you ready for continue?");
+console.log(shouldRenew);*/
+////////////////////////////
+
+/*const quantity = prompt('Input a number1, please:');
+console.log(quantity);
+console.log(typeof quantity);*/
+
+/*
+let quantity1 = prompt('Input a number2, please:');
+console.log(typeof quantity1);
+quantity1 = Number(quantity1);
+console.log(quantity1);
+console.log(typeof quantity1);
+
+let quantity2 = prompt('Input a number3, please:');
+console.log(typeof quantity2, quantity2);
+quantity2 = Number.parseInt(quantity2);
+console.log(typeof quantity2, quantity2);
+
+let quantity3 = prompt('Input a float number4, please:');
+console.log(typeof quantity3, quantity3);
+quantity3 = Number.parseFloat(quantity3);
+console.log(typeof quantity3, quantity3);*/
+///////////////////////////////
+
+let sal = 1342.345679;
+console.log(typeof sal, sal);
+sal = sal.toFixed(2);
+console.log(typeof sal, sal);
+sal = Number(sal);
+console.log(typeof sal, sal);
+
+///
+
+console.log(Math);
+console.log(Math.PI);
+
+const base = 2, power = 7;
+console.log(Math.pow(base, power));
+console.log(base ** power);
+console.log(typeof (base ** power));
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(20 + Math.random() * (50 - 20));
+
+const maxN = 50;
+const minN = 20;
+const resultNrandom = minN + Math.random() * (maxN - minN);
+const resultNrandRound = Math.round(resultNrandom);
+
+console.log(resultNrandom, resultNrandRound);
+
