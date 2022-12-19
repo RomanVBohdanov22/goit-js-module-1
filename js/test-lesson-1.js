a0147ca4172773822ca495da6976f496490ffc81
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-19 20:25:52 +0200

    training function else if

--- js/test-lesson-1.js
+++ js/test-lesson-1.js
@@ -391,4 +391,62 @@ console.log(makeTransaction(5000, 10, 8000));
 console.log(makeTransaction(2000, 8, 10000));
 console.log(makeTransaction(500, 10, 5000));
 
-console.log();
+console.log('');
+
+/*
+
+
+Конструкція if...else може перевірити і зреагувати на виконання або невиконання тільки однієї умови.
+
+let cost;
+const subscription = "pro";
+
+if (subscription === "pro") {
+  cost = 100;
+} else {
+  cost = 0;
+}
+
+console.log(cost); // 100
+
+Блок else...if дозволяє додати після else ще один оператор if з умовою. В кінці ланцюжка може бути класичний блок else, який виконається тільки у разі, коли жодна з умов не приведеться до true.
+
+let cost;
+const subscription = "premium";
+
+if (subscription === "free") {
+  cost = 0;
+} else if (subscription === "pro") {
+  cost = 100;
+} else if (subscription === "premium") {
+  cost = 500;
+} else {
+  console.log(`Invalid subscription type - ${subscription}`);
+}
+
+console.log(cost); // 500
+
+За першої ж задовільної умови перевірки припиняться і виконається тільки тіло відповідного блоку if. Тому подібний запис потрібно читати як: «шукаю перше виконання умови, ігнорую все інше».
+
+*/
+
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+
+  if (password === null) { // Change this line
+    message =  'Canceled by user!';
+  } else if (password === ADMIN_PASSWORD) { // Change this line
+    message = 'Welcome!';
+  } else {
+    message = 'Access denied, wrong password!';
+  }
+
+  return message;
+}
+
+console.log(checkPassword(null));
+console.log(checkPassword('jqueryismyjam'));
+console.log(checkPassword('advga'));
+console.log('');
