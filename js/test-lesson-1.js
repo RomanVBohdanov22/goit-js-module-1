1029d95b91491bdfafebca1e04fe261dc93c9e5b
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-22 06:20:09 +0200

    update test-lesson task24

--- js/test-lesson-1.js
+++ js/test-lesson-1.js
@@ -649,3 +649,83 @@ console.log(checkIfCanAccessContent('free'));
 
 console.log('');
 
+console.log('Завдання 23');
+/*
+
+
+Всі оператори, які ми розглядали до цього, були бінарними. Тобто вони містили два операнди:
+ лівий і правий. Логічне «НЕ» - це унарний оператор, що виконує операцію над одним операндом справа.
+
+!вираз
+
+Оператор ! приводить операнд до буля, якщо необхідно, а потім робить інверсію - 
+замінює його на протилежний true -> false або false -> true.
+
+console.log(!true); // false
+console.log(!false); // true
+console.log(!3); // !3 -> !true -> false
+console.log(!"Mango"); // !'Mango' -> !true -> false
+console.log(!0); // !0 -> !false -> true
+console.log(!""); // !'' -> !false -> true
+
+const isOnline = true;
+const isNotOnline = !isOnline; // !isOnline -> !true -> false
+
+
+*/
+
+function isNumberNotInRange(start, end, number) {
+  const isInRange = number >= start && number <= end;
+  const isNotInRange = !isInRange; // Change this line
+
+  return isNotInRange;
+}
+
+
+console.log(isNumberNotInRange(10, 30, 17));
+console.log(isNumberNotInRange(10, 30, 5));
+console.log(isNumberNotInRange(20, 50, 24));
+console.log(isNumberNotInRange(20, 50, 76));
+
+console.log('');
+
+console.log('Завдання 24'):
+/*
+Функція getDiscount(totalSpent) визначає значення знижки, 
+залежно від загальної суми витрачених грошей (параметр totalSpent) 
+в магазині за весь час (партнерська програма). Знижка записується у змінну 
+discount і повертається з функції як результат її роботи.
+
+Використовуючи розгалуження і логічні оператори, доповни код функції.
+
+    Якщо витрачено від 50000 (включно) або більше кредитів - знижка 10% (золотий партнер)
+    Якщо витрачено від 20000 (включно) до 50000 кредитів - знижка 5% (срібний партнер)
+    Якщо витрачено від 5000 (включно) до 20000 кредитів - знижка 2% (бронзовий партнер)
+    Якщо витрачено менше 5000 кредитів - знижка 0 (базовий партнер)
+
+Значення знижок кожного рівня зберігаються в однойменних константах BASE_DISCOUNT,
+ BRONZE_DISCOUNT, SILVER_DISCOUNT і GOLD_DISCOUNT.
+
+    Оголошена функція getDiscount(totalSpent)
+    Виклик getDiscount(137000) повертає 0.1
+    Виклик getDiscount(46900) повертає 0.05
+    Виклик getDiscount(8250) повертає 0.02
+    Виклик getDiscount(1300) повертає 0
+    Виклик getDiscount(5000) повертає 0.02
+    Виклик getDiscount(20000) повертає 0.05
+    Виклик getDiscount(50000) повертає 0.1
+
+ */
+
+function getDiscount(totalSpent) {
+  const BASE_DISCOUNT = 0;
+  const BRONZE_DISCOUNT = 0.02;
+  const SILVER_DISCOUNT = 0.05;
+  const GOLD_DISCOUNT = 0.1;
+  let discount;
+  // Change code below this line
+    
+
+  // Change code above this line
+  return discount;
+}
