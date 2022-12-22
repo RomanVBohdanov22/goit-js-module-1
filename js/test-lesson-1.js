27c52a22551e7938bf8e5a2429f52af2b896ba16
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-22 10:01:43 +0200

    all 36 was

--- js/test-lesson-1.js
+++ js/test-lesson-1.js
@@ -1345,3 +1345,103 @@ function normalizeInput(input) {
 Виклик функції normalizeInput('This ISN'T a SpaM') повертає 'this isn't a spam'
 Виклик функції normalizeInput('Big SALES') повертає 'big sales'
 */
+
+console.log('Task 35');
+
+/*
+
+
+Метод рядків includes(substring) перевіряє, чи входить підрядок substring у рядок, повертає буль - true, якщо входить, і false - в іншому випадку. Регістр символів в рядку і підрядку має значення, оскільки, наприклад літера "a" не дорівнює літері "А".
+
+const productName = "Repair droid";
+
+console.log(productName.includes("a")); // true
+console.log(productName.includes("A")); // false
+console.log(productName.includes("droid")); // true
+console.log(productName.includes("Droid")); // false
+console.log(productName.includes("Repair")); // true
+console.log(productName.includes("repair")); // false
+
+
+
+*/
+/*
+Функція checkForName(fullname, name) приймає два параметри та повертає буль true або false - результат 
+перевірки входження підрядка name у рядок fullname.
+
+    fullname - повне ім'я, що складається з двох слів (імені та прізвища), розділених пробілом.
+    name - ім'я для перевірки входження в повне ім'я.
+
+Присвой змінній result вираз перевірки входження імені (параметр name), у повне ім'я (параметр fullname). 
+Нехай функція чітко розрізняє регістр літер, тобто «Петя» і «петя» - для неї різні імена.
+
+    Оголошена функція checkForName(fullname, name).
+    Виклик функції checkForName("Egor Kolbasov", "Egor") повертає true
+    Виклик функції checkForName("Egor Kolbasov", "egor") повертає false
+    Виклик функції checkForName("Egor Kolbasov", "egOr") повертає false
+    Виклик функції checkForName("Egor Kolbasov", "Zhenya") повертає false
+    Виклик функції checkForName("Vadim Nekrasov", "Vadim") повертає true
+    Виклик функції checkForName("Vadim Nekrasov", "vadim") повертає false
+    Виклик функції checkForName("Vadim Nekrasov", "Dima") повертає false
+
+*/
+
+
+function checkForName(fullName, name) {
+ const result = fullName.includes(name); // Change this line
+  return result;
+}
+
+/*
+Очікується оголошення 'Function Declaration'
+Функції присвоєно ім'я 'checkForName'
+Функція 'checkForName' має два параметри: 1-й - 'fullName' і 2-й - 'name'
+Виклик функції checkForName('Egor Kolbasov', 'Egor') повертає true
+Виклик функції checkForName('Egor Kolbasov', 'egor') повертає false
+Виклик функції checkForName('Egor Kolbasov', 'egOr') повертає false
+Виклик функції checkForName('Egor Kolbasov', 'Zhenya') повертає false
+Виклик функції checkForName('Vadim Nekrasov', 'Vadim') повертає true
+Виклик функції checkForName('Vadim Nekrasov', 'vadim') повертає false
+Виклик функції checkForName('Vadim Nekrasov', 'Dima') повертає false
+*/
+
+console.log('Task 36');
+/*Задача: перевірка спаму*/
+
+/*
+Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути у довільному регістрі, наприклад SPAM або sAlE.
+
+    Якщо знайшли заборонене слово (spam або sale) то функція повертає буль true.
+    Якщо в рядку відсутні заборонені слова, функція повертає буль false.
+
+    Оголошена функція checkForSpam(message).
+    Виклик функції checkForSpam("Latest technology news") повертає false
+    Виклик функції checkForSpam("JavaScript weekly newsletter")повертає false
+    Виклик функції checkForSpam("Get best sale offers now!") повертає true
+    Виклик функції checkForSpam("Amazing SalE, only tonight!") повертає true
+    Виклик функції checkForSpam("Trust me, this is not a spam message") повертає true
+    Виклик функції checkForSpam("Get rid of sPaM emails. Our book in on sale!") повертає true
+    Виклик функції checkForSpam("[SPAM] How to earn fast money?") повертає true
+
+*/
+
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+    result = ((message.toLowerCase()).includes('spam'))||((message.toLowerCase()).includes('sale'));
+  // Change code above this line
+  return result;
+}
+
+/*
+Очікується оголошення 'Function Declaration'
+Функції присвоєно ім'я 'checkForSpam'
+Функція 'checkForSpam' має один параметр 'message'
+Виклик функції checkForSpam('Latest technology news') повертає false
+Виклик функції checkForSpam('JavaScript weekly newsletter') повертає false
+Виклик функції checkForSpam('Get best sale offers now!') повертає true
+Виклик функції checkForSpam('Amazing SalE, only tonight!') повертає true
+Виклик функції checkForSpam('Trust me, this is not a spam message') повертає true
+Виклик функції checkForSpam('Get rid of sPaM emails. Our book in on sale!') повертає true
+Виклик функції checkForSpam('[SPAM] How to earn fast money?') повертає true
+*/
