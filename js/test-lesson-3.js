2056fc7b9df29a6a96e1abc08abd9001dc9f0a6c
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-27 01:06:20 +0200

    t3.2

--- js/test-lesson-3.js
+++ js/test-lesson-3.js
@@ -53,3 +53,73 @@ console.log(apartment.descr);
 console.log(apartment.rating);
 console.log(apartment.price);
 console.log(apartment.tags[0], apartment.tags[1], apartment.tags[2]);
+
+
+console.log('');
+console.log('tsk 3.2');
+
+/*
+
+Значенням властивості може бути інший об'єкт. Це використовується для зберігання вкладених і згрупованих даних.
+
+Наприклад, статистика користувача соціальної мережі складається з кількості послідовників, переглядів і лайків, 
+і зберігати ці дані найзручніше у вигляді об'єкта. Те саме з місцем розташування, окремо країна і місто.
+
+В майбутньому це можна буде використовувати для пошуку користувачів за країною, містом, мінімальною або максимальною 
+кількістю послідовників тощо.
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "jgluke",
+  location: {
+    country: "Jamaica",
+    city: "Ocho Rios",
+  },
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+*/
+
+/*
+Доповни об'єкт квартири властивістю owner, значенням якого буде об'єкт з інформацією про власника. Додай йому наступні властивості:
+
+    name - ім'я власника, значення "Henry";
+    phone - телефон, значення "982-126-1588";
+    email - пошта, значення "henry.carter@aptmail.com".
+
+    Оголошена змінна apartment
+    Значення змінної apartment - це об'єкт
+    В об'єкта змінної apartment присутні властивості imgUrl, descr, rating, price і tag зі значеннями
+    В об'єкті apartment присутня властивість owner
+    Значення властивості owner - це об'єкт
+    В об'єкті owner присутня властивість name
+    Значення властивості name - це "Henry"
+    В об'єкті owner присутня властивість phone
+    Значення властивості phone - це "982-126-1588"
+    В об'єкті owner присутня властивість email
+    Значення властивості email - це "henry.carter@aptmail.com"
+
+*/
+
+const apartment2 = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+    tags: ["premium", "promoted", "top"],
+    owner: {
+        name: "Henry",
+        phone: "982-126-1588",
+        email: "henry.carter@aptmail.com",
+    }
+};
+/*Оголошена змінна 'apartment'
+Значення змінної 'apartment' - це об'єкт
+В об'єкта змінної 'apartment' присутні властивості 'imgUrl', 'descr', 'rating', 'price' і 'tag' зі значеннями
+Значення властивості 'owner' - це об'єкт
+В об'єкті 'owner' присутня властивість 'name'. Значення властивості 'name' - це 'Henry'
+В об'єкті 'owner' присутня властивість phone'. Значення властивості 'phone' - це '982-126-1588'.
+В об'єкті 'owner' присутня властивість 'email'. Значення властивості 'email' - це 'henry.carter@aptmail.com'. */
