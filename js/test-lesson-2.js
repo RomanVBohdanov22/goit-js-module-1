29e6b249173865852b870219bd047af936704740
RomanVBohdanov22 <semsterfx18@gmail.com>
2022-12-26 20:55:16 +0200

    task. 2.32 fin

--- js/test-lesson-2.js
+++ js/test-lesson-2.js
@@ -1444,3 +1444,53 @@ console.log(findNumber(16, 35, 7));
 В циклі for не повинен використовуватися `break` для виходу до завершення всіх ітерацій циклу
 */
 
+
+console.log('');
+console.log('tsk 2.32');
+
+/*
+Напиши функцію includes(array, value), яка робить те саме, що і метод масиву масив.includes(значення) - 
+перевіряє, чи присутнє в масиві array значення value, повертаючи true, якщо присутнє, і false в іншому випадку.
+
+При виконанні цього завдання в тілі функції includes() не можна використовувати метод масив.includes(значення).
+
+    Оголошена функція includes(array, value)
+    Виклик includes([1, 2, 3, 4, 5], 3) повертає true
+    Виклик includes([1, 2, 3, 4, 5], 17) повертає false
+    Виклик includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") повертає true
+    Виклик includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") повертає false
+    Виклик includes(["apple", "plum", "pear", "orange"], "plum") повертає true
+    Виклик includes(["apple", "plum", "pear", "orange"], "kiwi") повертає false
+    Виклик includes() для випадкового масиву з випадковим value повертає правильний boolean
+    У функції includes використовується for, return, але не метод масиву includes
+
+*/
+
+function includes(array, value) {
+  // Change code below this line
+  for (const arrayValue of array) {
+    if (arrayValue === value) return true;
+  }
+  return false;
+  // Change code above this line
+}
+
+console.log(includes([1, 2, 3, 4, 5], 3));
+
+console.log(includes([1, 2, 3, 4, 5], 17));
+
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+
+/* 
+ Очікується оголошення 'Function Declaration'
+Функції присвоєно ім'я 'includes'
+Функція 'includes' має два параметри: 'array' і 'value'
+Виклик includes([1, 2, 3, 4, 5], 3) повертає true
+Виклик includes([1, 2, 3, 4, 5], 17) повертає false
+Виклик includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Jupiter') повертає true
+Виклик includes(['Earth', 'Mars', 'Venus', 'Jupiter', 'Saturn'], 'Uranus') повертає false
+Виклик includes(['apple', 'plum', 'pear', 'orange'], 'plum') повертає true
+Виклик includes(['apple', 'plum', 'pear', 'orange'], 'kiwi') повертає false
+Виклик `includes()` з випадковим масивом і випадковим 'value' повертає правильний результат
+У функції 'includes' використовується 'for', 'return', але не метод масиву 'includes'
+*/
