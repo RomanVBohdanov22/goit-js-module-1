f9b6cb67725e1d5fea5fae2d9cfc0c333ff8410e
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-02 15:30:47 +0200

    10 task

--- js/test-lesson-3.js
+++ js/test-lesson-3.js
@@ -691,3 +691,72 @@ const credentials = {
 
 console.log('');
 console.log('tsk 3.10');
+
+/*
+На відміну від масиву або рядка, об'єкт - це не ітерабельна сутність, 
+тобто його не можна перебрати циклами for або for...of. Для перебирання об'єктів використовується 
+спеціальний цикл for...in, який перебирає ключі об'єкта object.
+
+for (key in object) {
+  // інструкції
+}
+
+Змінна key доступна тільки в тілі циклу. На кожній ітерації в неї буде записано значення ключа 
+(ім'я) властивості. 
+Для того щоб отримати значення властивості з таким ключем (ім'ям), використовується синтаксис квадратних дужок.
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+
+for (const key in book) {
+  // Ключ
+  console.log(key);
+  // Значення властивості з таким ключем
+  console.log(book[key]);
+}
+
+*/
+
+/*
+Перебери об'єкт apartment, використовуючи цикл for...in, і запиши в масив keys всі його ключі, 
+а в масив values всі значення його властивостей.
+
+    Оголошена змінна apartment
+    Значення змінної apartment - це об'єкт
+    Оголошена змінна keys
+    Значення змінної keys - це масив ["descr", "rating", "price"]
+    Оголошена змінна values
+    Значення змінної values - це масив ["Spacious apartment in the city center", 4, 2153]
+
+
+*/
+
+const apartment10 = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (const key in apartment10) {
+  // Ключ
+    keys.push(key);
+  console.log(key);
+  // Значення властивості з таким ключем
+    values.push(apartment10[key]);
+  console.log(apartment10[key]);
+}
+/*
+ Оголошена змінна 'apartment'
+Значення змінної 'apartment' - це об'єкт
+Оголошена змінна 'keys'
+Значення змінної 'keys' - це масив ['descr', 'rating', 'price']
+Оголошена змінна 'values'
+Значення змінної 'values' - це масив ['Spacious apartment in the city center', 4, 2153]
+Використовується цикл 'for...in'
+*/
