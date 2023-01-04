974072bef5b7db8bf2cb18807f1ae375099aed23
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-04 17:11:56 +0200

    3-19 t0 3-20

--- js/test-lesson-3-13.js
+++ js/test-lesson-3-13.js
@@ -215,3 +215,220 @@ console.log(countTotalSalary16({}));
 console.log(countTotalSalary16({ mango: 100, poly: 150, alfred: 80 }));
 
 console.log(countTotalSalary16({ kiwi: 200, poly: 50, ajax: 150 }));
+
+
+
+
+console.log('');
+console.log('tsk 3.17');
+/*
+У стандартний набір повсякденних завдань розробника входить маніпуляція масивом однотипних об'єктів.
+ Це означає, що всі об'єкти в масиві гарантовано матимуть однаковий набір властивостей, але з різними значеннями.
+
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+];
+
+Для перебирання такого масиву використовується стандартний цикл for...of. 
+Значення властивостей кожного об'єкта можна отримати, використовуючи синтаксис «через крапку», оскільки в кожному об'єкті
+ набір властивостей та їх імена будуть однакові, відрізняються тільки значення.
+
+for (const book of books) {
+  // Об'єкт книги
+  console.log(book);
+  // Назва
+  console.log(book.title);
+  // Автор
+  console.log(book.author);
+  // Рейтинг
+  console.log(book.rating);
+}
+*/
+/*
+Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, 
+а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+    Оголошена змінна colors
+    Значення змінної colors - це масив
+    Оголошена змінна hexColors
+    Значення змінної hexColors - це масив ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+    Оголошена змінна rgbColors
+    Значення змінної rgbColors - це масив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
+
+*/
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+
+for (const color of colors) { 
+    hexColors.push(color.hex);
+    rgbColors.push(color.rgb);
+}
+
+console.log(hexColors);
+console.log(rgbColors);
+
+/*
+Оголошена змінна 'colors'
+Значення змінної 'colors' - це масив
+Оголошена змінна 'hexColors'
+Значення змінної 'hexColors' - це масив ['#f44336', '#2196f3', '#4caf50', '#ffeb3b']
+Оголошена змінна 'rgbColors'
+Значення змінної 'rgbColors' - це масив ['244,67,54', '33,150,243', '76,175,80', '255,235,59']
+Значення змінних 'hexColors' і 'rgbColors' отримані за допомогою циклу 'for...of'
+*/
+
+
+console.log('');
+console.log('tsk 3.18');
+//Задача. Пошук об'єкта за значенням властивості
+/*
+Напиши функцію getProductPrice(productName), яка приймає один параметр productName - назва продукту. 
+Функція шукає об'єкт продукту з таким ім'ям (властивість name) в масиві products і повертає його ціну (властивість price).
+ Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+
+    Оголошена функція getProductPrice(productName).
+    Виклик getProductPrice("Radar") повертає 1300.
+    Виклик getProductPrice("Grip") повертає 1200.
+    Виклик getProductPrice("Scanner") повертає 2700.
+    Виклик getProductPrice("Droid") повертає 400.
+    Виклик getProductPrice("Engine") повертає null.
+*/
+
+const products18 = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(productName) {
+  // Change code below this line
+    for (const productItem of products18) { 
+        if (productItem.name === productName) return productItem.price;
+    }
+    return null;
+  // Change code above this line
+}
+
+console.log(getProductPrice("Radar"));
+console.log(getProductPrice("Grip"));
+console.log(getProductPrice("Scanner"));
+console.log(getProductPrice("Droid"));
+console.log(getProductPrice("Engine"));
+/*
+Оголошена функція getProductPrice(productName)
+Виклик getProductPrice('Radar') повертає 1300
+Виклик getProductPrice('Grip') повертає 1200
+Виклик getProductPrice('Scanner') повертає 2700
+Виклик getProductPrice('Droid') повертає 400
+Виклик getProductPrice('Engine') повертає null
+*/
+
+console.log('');
+console.log('tsk 3.19');
+//Задача: колекція значень властивості
+/*
+Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. 
+Функція повинна повернути масив всіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. 
+Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+    Оголошена функція getAllPropValues(propName)
+    Виклик getAllPropValues("name") повертає ["Radar", "Scanner", "Droid", "Grip"]
+    Виклик getAllPropValues("quantity") повертає [4, 3, 7, 9]
+    Виклик getAllPropValues("price") повертає [1300, 2700, 400, 1200]
+    Виклик getAllPropValues("category") повертає []
+*/
+
+const products19 = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  // Change code below this line
+    let collectionFromPropName = [];
+    for (const productItem of products19) { 
+        if ((Object.keys(productItem)).includes(propName))
+        { 
+            //console.log(Object.values(productItem));
+            for (const key in productItem) {
+                if (key === propName) collectionFromPropName.push(productItem[key]);
+             }
+        }
+        
+    }
+    return collectionFromPropName;
+
+
+  // Change code above this line
+}
+
+console.log(getAllPropValues("name"));
+console.log(getAllPropValues("quantity"));
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("category"));
+
+/*
+Оголошена функція getAllPropValues(propName)
+Ваш код занадто довго виконується
+Виклик getAllPropValues('quantity') повертає [4, 3, 7, 9]
+Виклик getAllPropValues('price') повертає [1300, 2700, 400, 1200]
+Виклик getAllPropValues('category') повертає []
+*/
+
+
+console.log('');
+console.log('tsk 3.20');
+//Задача: загальна вартість товару
+
+/*
+Напиши функцію calculateTotalPrice(productName), яка приймає один параметр productName - назва товару.
+ Функція повинна повернути загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+    Оголошена функція calculateTotalPrice(productName)
+    Виклик calculateTotalPrice("Blaster") повертає 0
+    Виклик calculateTotalPrice("Radar") повертає 5200
+    Виклик calculateTotalPrice("Droid") повертає 2800
+    Виклик calculateTotalPrice("Grip") повертає 10800
+    Виклик calculateTotalPrice("Scanner") повертає 8100
+
+*/
+const products20 = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  // Change code below this line
+
+
+
+  // Change code above this line
+}
