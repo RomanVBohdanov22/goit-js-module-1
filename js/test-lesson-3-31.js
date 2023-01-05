d1c8f098c7eedf805eeb2d1da59bb297ad334204
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-06 01:31:10 +0200

    3.35e

--- js/test-lesson-3-31.js
+++ js/test-lesson-3-31.js
@@ -375,7 +375,8 @@ const bookShelf35 = {
   books: ["The last kingdom", "Haze", "The guardian of dreams"],
   updateBook(oldName, newName) {
     // Change code below this line
-
+    const bookIndex = this.books.indexOf(bookName);
+    this.books.splice(bookIndex, 1);
 
 
     // Change code above this line
