2f4ca02251f598f40891b407456f5f77e155b20f
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-11 21:44:58 +0200

    tsk 4.8 started

--- js/test-lasson-4-05.js
+++ js/test-lasson-4-05.js
@@ -101,10 +101,9 @@ console.log(greet); // ƒ greet() { return `Ласкаво просимо ${name
 
 
 
-
-//Задача. Фільтрація масиву чисел
 console.log('');
 console.log('tsk 4.06');
+//Задача. Фільтрація масиву чисел
 /*
 Функція filterArray(numbers, value) приймає масив чисел numbers і повертає новий масив, 
 в якому будуть тільки ті елементи оригінального масиву, які більші за значення параметра value.
@@ -153,3 +152,67 @@ console.log(filterArray([12, 24, 8, 41, 76], 20));
 Результатом виклику функції filterArray([9,14,18,9,13,7,24,19,12,8,23,23,18,24,15,5,23,10,18,8,8,19,9,7], 15)) 
 буде [18,24,19,23,23,18,24,23,18,19]
 */
+
+console.log('');
+console.log('tsk 4.07');
+//Задача. Спільні елементи
+
+/*
+Функція getCommonElements(firstArray, secondArray) приймає два масиви довільної 
+довжини в параметри firstArray і secondArray, і повертає новий масив їхніх спільних елементів, 
+тобто тих, які присутні в обох масивах.
+
+Виконай рефакторинг функції таким чином, щоб замість циклу for, вона використовувала метод forEach.
+
+    Оголошена функція getCommonElements(firstArray, secondArray)
+    Для перебирання параметра (масиву) використаний метод forEach
+    Виклик getCommonElements([1, 2, 3], [2, 4]) повертає [2]
+    Виклик getCommonElements([1, 2, 3], [2, 1, 17, 19]) повертає [1, 2]
+    Виклик getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) повертає [12, 27, 3]
+    Виклик getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) повертає [10, 30, 40]
+    Виклик getCommonElements([1, 2, 3], [10, 20, 30]) повертає []
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+*/
+
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
+  // Change code below this line
+
+    firstArray.forEach(function(pickValue)
+    { 
+        if (secondArray.includes(pickValue)) commonElements.push(pickValue);
+    });
+  /*for (let i = 0; i < firstArray.length; i += 1) {
+    if (secondArray.includes(firstArray[i])) {
+      commonElements.push(firstArray[i]);
+    }
+  }*/
+
+  return commonElements;
+  // Change code above this line
+}
+
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
+console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+
+/*
+ Оголошена функція `getCommonElements(firstArray, secondArray)`
+Для перебирання масиву `firstArray` використаний метод `forEach`
+Виклик функції `getCommonElements([1, 2, 3], [2, 4])` повертає `[2]
+Виклик функції `getCommonElements([1, 2, 3], [2, 1, 17, 19])` повертає `[1, 2]`
+Виклик функції `getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27])` повертає `[12, 27, 3]`
+Виклик функції `getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40])` повертає `[10, 30, 40]`
+Виклик функції `getCommonElements([1, 2, 3], [10, 20, 30])` повертає `[]`
+Виклик функції з випадковими, але валідними аргументами, повертає правильне значення. Результатом виклику функції 
+getCommonElements([11,21,24,24,18,24,11,11,8,16,9,22,22,16,21,23,7,14,22,11,17,10], 
+    [7,14,6,9,11,13,19,9,20,7,12,8,24,7,24,11,24,8,14,5,12,19]) буде [11,24,24,24,11,11,8,9,7,14,11]
+*/
+
+console.log('');
+console.log('tsk 4.08');
+
