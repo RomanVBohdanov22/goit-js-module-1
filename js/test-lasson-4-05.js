747ebac272f3b1b90913cf2d5e73d9ba688bb30c
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-12 00:10:26 +0200

    4.11 task

--- js/test-lasson-4-05.js
+++ js/test-lasson-4-05.js
@@ -450,3 +450,35 @@ console.log(calculateTotalPriceA([412, 371, 94, 63, 176]));
 
 console.log('');
 console.log('tsk 4.11');
+
+//Задача. Фільтрація масиву чисел 2.0
+
+/*
+Заміни оголошення функції filterArray() і колбек для методу forEach() на стрілочні функції.
+
+    Оголошена змінна filterArray
+    Змінній filterArray присвоєна стрілочна функція з параметрами (numbers, value)
+    Для перебирання масиву numbers використаний метод forEach
+    Колбек для методу forEach - це стрілочна функція
+    Виклик функції filterArray([1, 2, 3, 4, 5], 3) повертає [4, 5]
+    Виклик функції filterArray([1, 2, 3, 4, 5], 4) повертає [5]
+    Виклик функції filterArray([1, 2, 3, 4, 5], 5) повертає []
+    Виклик функції filterArray([12, 24, 8, 41, 76], 38) повертає [41, 76]
+    Виклик функції filterArray([12, 24, 8, 41, 76], 20) повертає [24, 41, 76]
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+*/
+
+// Change code below this line
+function filterArrayB(numbers, value) {
+  const filteredNumbers = [];
+
+  numbers.forEach(function (number) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+
+  // Change code above this line
+  return filteredNumbers;
+}
