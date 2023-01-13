ca1632e6dca11dc64263709dd659acdb22af043f
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-13 16:22:15 +0200

    4.20 fin

--- js/test-lasson-4-05.js
+++ js/test-lasson-4-05.js
@@ -1286,3 +1286,108 @@ console.log(oddNumbers);
 
 console.log('');
 console.log('tsk 4.20');
+
+/*
+
+
+Використовуючи метод filter(), можна виконати фільтрацію масиву таким чином, що у ньому залишаться 
+тільки унікальні елементи. Цей прийом працює тільки з масивом примітивних значень - не об'єктів.
+
+Повернемося до групи студентів і масиву усіх відвідуваних предметів, які ми отримали методом flatMap().
+
+const students = [
+  { name: "Mango", courses: ["mathematics", "physics"] },
+  { name: "Poly", courses: ["science", "mathematics"] },
+  { name: "Kiwi", courses: ["physics", "biology"] },
+];
+
+const allCourses = students.flatMap(student => student.courses);
+// ["mathematics", "physics", "science", "mathematics", "physics", "biology"];
+
+У змінній allCourses зберігається масив усіх відвідуваних предметів, які можуть повторюватися. 
+Завдання полягає у тому, щоб створити новий масив, в якому будуть тільки унікальні предмети, тобто без повторень.
+
+const uniqueCourses = allCourses.filter(
+  (course, index, array) => array.indexOf(course) === index
+);
+
+Використовуючи array.indexOf(course), виконуємо пошук першого збігу поточного елемента course і 
+отримуємо його індекс в оригінальному масиві усіх курсів. В параметрі index зберігається індекс поточного 
+елемента course, перебираючи масив методом filter.
+
+Якщо результат indexOf() і значення index рівні - це унікальний елемент, тому що таке значення 
+зустрічається в масиві вперше, і на поточній ітерації фільтр обробляє саме його.
+
+# Масив усіх курсів
+["mathematics", "physics", "science", "mathematics", "physics", "biology"];
+
+Для елемента "mathematics" під індексом 0:
+
+    indexOf() поверне 0, тому що шукає перший збіг.
+    Значення параметра index буде 0.
+    Вони рівні, а отже, це унікальний елемент.
+
+Для елемента "mathematics" під індексом 3:
+
+    indexOf() поверне 0, тому що шукає перший збіг.
+    Значення параметра index буде 3.
+    Вони не рівні, а отже, це повторюваний - не унікальний елемент.
+
+
+*/
+/*
+Доповни код таким чином, щоб у змінній allGenres був масив всіх жанрів книг (властивість genres) 
+з масиву books, а у змінній uniqueGenres - масив унікальних жанрів, без повторень.
+
+    Оголошена змінна books
+    Значення змінної books - це масив об'єктів
+    Оголошена змінна allGenres
+    Значення змінної allGenres - це масив ["adventure", "history", "fiction", "mysticism", "horror", "mysticism", "adventure"]
+    Оголошена змінна uniqueGenres
+    Значення змінної uniqueGenres - це масив ["adventure", "history", "fiction", "mysticism", "horror"]
+    Для обчислення значення змінної allGenders використаний метод flatMap()
+    Для обчислення значення змінної uniqueGenres використаний метод filter()
+
+
+*/
+
+const books20 = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["adventure", "history"],
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    genres: ["fiction", "mysticism"],
+  },
+  {
+    title: "Redder Than Blood",
+    author: "Tanith Lee",
+    genres: ["horror", "mysticism", "adventure"],
+  },
+];
+
+/*
+const uniqueCourses = allCourses.filter(
+  (course, index, array) => array.indexOf(course) === index
+);
+ */
+// Change code below this line
+const allGenres = books20.flatMap(books20 => books20.genres);
+const uniqueGenres = allGenres.filter( (genre, index, array) =>  array.indexOf(genre) === index);
+
+console.log(allGenres);
+console.log(uniqueGenres);
+
+/*
+Оголошена змінна `books`
+Оголошена змінна `allGenres`
+Оголошена змінна `uniqueGenres`
+Значення змінної `books` - це масив об'єктів
+Значення змінної `allGenres` - це масив `['adventure', 'history', 'fiction', 'mysticism', 'horror', 'mysticism', 'adventure']`
+Значення змінної `uniqueGenres` - це масив `['adventure', 'history', 'fiction', 'mysticism', 'horror']`
+Для обчислення значення змінної `allGenders` використаний метод `flatMap()`
+Для обчислення значення змінної `uniqueGenres` використаний метод `filter()`
+*/
