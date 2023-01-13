0bf912d319c02369a737bac3b28e1face18fbe61
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-13 23:13:34 +0200

    tsk 4.27

--- js/test-lesson-4-21.js
+++ js/test-lesson-4-21.js
@@ -577,3 +577,127 @@ console.log(getFriends(testUsers25));
  'Goldie Gentry', 'Aisha Tran', 'Jordan Sampson', 'Eddie Strong', 'Jacklyn Lucas', 'Linda Chapman']`
 Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
 */
+
+console.log('');
+console.log('tsk 4.26');
+
+const testUsers26 = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39
+  }
+];
+
+/*
+Доповни функцію getActiveUsers(users) таким чином, щоб вона повертала масив активних користувачів, 
+значення властивості isActive яких - true.
+
+    Оголошена змінна getActiveUsers Змінній getActiveUsers присвоєна стрілочна функція з параметром users
+    Для перебирання параметра users використовується метод filter()
+    Виклик функції із зазначеним масивом користувачів повертає масив об'єктів користувачів 
+    з іменами Sharlene Bush, Elma Head, Carey Barr і Sheree Anthony
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+*/
+//const getUsersWithFriend = (users, friendName) => users.filter(({ friends }) => friends.includes(friendName));
+//const getUsersWithEyeColor = (users, color) => users.filter(({eyeColor}) => eyeColor === color);
+// Change code below this line
+const getActiveUsers = (users) => users.filter(({isActive}) => isActive);
+// Change code above this line
+
+console.log(getActiveUsers(testUsers26));
+
+/*
+ Оголошена змінна `getActiveUsers`
+Змінній `getActiveUsers` присвоєна стрілочна функція з параметром `(users)`
+Для перебирання параметра `users` використаний метод `filter()`
+Виклик функції із зазначеним масивом користувачів повертає масив об'єктів користувачів
+ з іменами `Sharlene Bush`, `Elma Head`, `Carey Barr` і `Sheree Anthony`
+Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+*/
+
+console.log('');
+console.log('tsk 4.27');
+
+/*
+Доповни функцію getInactiveUsers(users) таким чином, щоб вона повертала масив неактивних користувачів, 
+значення властивості isActive яких - false.
+
+    Оголошена змінна getInactiveUsers.
+    Змінній getInactiveUsers присвоєна стрілочна функція з параметром users
+    Для перебирання параметра users використовується метод filter()
+    Виклик функції із зазначеним масивом користувачів повертає масив об'єктів користувачів з іменами Moore Hensley, 
+    Ross Vazquez і Blackburn Dotson
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+*/
+
+const getInactiveUsers = (users) => users.filter(({ isActive }) => !isActive);
+
+console.log(getInactiveUsers(testUsers26));
