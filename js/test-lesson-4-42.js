3451b28e63ef823d6bf1639601eb3cdc36caec4f
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-16 01:04:46 +0200

    task 4.47 fin

--- js/test-lesson-4-42.js
+++ js/test-lesson-4-42.js
@@ -423,4 +423,229 @@ const books = [
 const MIN_BOOK_RATING = 8;
 // Change code below this line
 
-const names = books;
+const names = [...books].sort((firstBook, secondBook) => (firstBook.author).localeCompare(secondBook.author)).
+    filter(sortedBook => sortedBook.rating > MIN_BOOK_RATING).map(book => book.author);
+
+
+console.log(names);
+
+/*
+Оголошена змінна `books`
+Оголошена змінна `MIN_BOOK_RATING`
+Оголошена змінна `names`
+Значення змінної `books` - це вихідний масив об'єктів
+Значення змінної `MIN_BOOK_RATING` - це число `8`
+Значення змінної `names` - це масив `['Bernard Cornwell', 'Howard Lovecraft', 'Robert Sheckley']`
+Відсутні оголошені змінні, крім `books`, `MIN_BOOK_RATING` і `names`
+Використовується ланцюжок методів `filter`, `map`, `sort`
+*/
+
+
+console.log('');
+console.log('tsk 4.46');
+
+const testUsers46 = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male"
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female"
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male"
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female"
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male"
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+    isActive: false,
+    balance: 1498,
+    gender: "male"
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female"
+  }
+]
+
+/*
+Доповни функцію getNamesSortedByFriendCount(users) таким чином, щоб вона повертала масив 
+імен користувачів, відсортований за зростанням кількості їхніх друзів (властивість friends).
+
+    Оголошена змінна getNamesSortedByFriendCount
+    Змінній getNamesSortedByFriendCount присвоєна стрілочна функція з параметром (users)
+    У тілі функції використовується ланцюжок методів
+    Значення параметра users не змінюється
+    Виклик функції із зазначеним масивом користувачів повертає 
+    масив ["Moore Hensley", "Sharlene Bush", "Elma Head", "Sheree Anthony", "Ross Vazquez", "Carey Barr", "Blackburn Dotson"]
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+*/
+/*
+const getTotalFriendCount = users => {
+    return users.reduce((friendsCount, user) =>
+    {return (friendsCount + user.friends.length)}, 0);
+};
+*/
+//[...books].sort((firstBook, secondBook) => (firstBook.author).localeCompare(secondBook.author))
+
+// Change code below this line
+const getNamesSortedByFriendCount = users => {
+    return [...users].sort((firstUser, secondUser) => firstUser.friends.length - secondUser.friends.length).
+    map(userNamed => userNamed.name);
+};
+// Change code above this line
+
+console.log(getNamesSortedByFriendCount(testUsers46));
+//(7) ['Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Sheree Anthony', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson']
+
+/*Оголошена змінна `getNamesSortedByFriendCount`
+Змінній `getNamesSortedByFriendCount` присвоєна стрілочна функція з параметром `(users)`
+У тілі функції використовується ланцюжок методів
+Значення параметра `users` не змінюється
+Виклик функції із зазначеним масивом користувачів повертає масив `['Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Sheree Anthony', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson']`
+Виклик функції з випадковими, але валідними аргументами, повертає правильне значення */
+
+
+console.log('');
+console.log('tsk 4.47');
+
+const testUsers47 = [
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        eyeColor: "blue",
+        friends: ["Sharron Pace"],
+        isActive: false,
+        balance: 2811,
+        gender: "male"
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        eyeColor: "blue",
+        friends: ["Briana Decker", "Sharron Pace"],
+        isActive: true,
+        balance: 3821,
+        gender: "female"
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        eyeColor: "green",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        isActive: false,
+        balance: 3793,
+        gender: "male"
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        eyeColor: "green",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        isActive: true,
+        balance: 2278,
+        gender: "female"
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        eyeColor: "blue",
+        friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+        isActive: true,
+        balance: 3951,
+        gender: "male"
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        eyeColor: "brown",
+        friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+        isActive: false,
+        balance: 1498,
+        gender: "male"
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        eyeColor: "brown",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        isActive: true,
+        balance: 2764,
+        gender: "female"
+    }
+];
+
+/*
+
+Доповни функцію getSortedFriends(users) таким чином, щоб вона повертала масив унікальних імен друзів (властивість friends), 
+відсортований за алфавітом.
+
+    Оголошена змінна getSortedFriends
+    Змінній getSortedFriends присвоєна стрілочна функція з параметром (users)
+    У тілі функції використовується ланцюжок методів в правильному порядку
+    Значення параметра users не змінюється
+    Виклик функції із зазначеним масивом користувачів повертає масив ["Adrian Cross", "Aisha Tran", "Briana Decker", "Eddie Strong", "Goldie Gentry", "Jacklyn Lucas", "Jordan Sampson", "Linda Chapman", "Marilyn Mcintosh", "Naomi Buckner", "Padilla Garrison", "Sharron Pace", "Solomon Fokes"]
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+
+*/
+
+// Change code below this line
+const getSortedFriends = users => {
+    return [...users].flatMap(users => users.friends).filter((friend, index, array) => array.indexOf(friend) === index).
+    sort((firstFriend, secondFriend) => (firstFriend).localeCompare(secondFriend));
+};
+// Change code above this line
+
+console.log(getSortedFriends(testUsers47));
+//(13) ['Adrian Cross', 'Aisha Tran', 'Briana Decker', 'Eddie Strong', 'Goldie Gentry', 'Jacklyn Lucas', 'Jordan Sampson', 'Linda Chapman', 'Marilyn Mcintosh', 'Naomi Buckner', 'Padilla Garrison', 'Sharron Pace', 'Solomon Fokes']
+/* Оголошена змінна `getSortedFriends`
+Змінній `getSortedFriends` присвоєна стрілочна функція з параметром `(users)`
+У тілі функції використовується ланцюжок методів в правильному порядку
+Значення параметра `users` не змінюється
+Виклик функції із зазначеним масивом користувачів повертає масив `['Adrian Cross', 'Aisha Tran', 'Briana Decker', 'Eddie Strong', 'Goldie Gentry', 'Jacklyn Lucas', 'Jordan Sampson', 'Linda Chapman', 'Marilyn Mcintosh', 'Naomi Buckner', 'Padilla Garrison', 'Sharron Pace', 'Solomon Fokes']`
+Виклик функції з випадковими, але валідними аргументами, повертає правильне значення */
+
