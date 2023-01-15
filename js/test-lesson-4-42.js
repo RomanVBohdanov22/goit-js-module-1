3cfaade3119e9387ce6fc29c75653d20f7ee13d4
RomanVBohdanov22 <semsterfx18@gmail.com>
2023-01-16 01:26:33 +0200

    test-lesson 4-48

--- js/test-lesson-4-42.js
+++ js/test-lesson-4-42.js
@@ -649,3 +649,117 @@ console.log(getSortedFriends(testUsers47));
 Виклик функції із зазначеним масивом користувачів повертає масив `['Adrian Cross', 'Aisha Tran', 'Briana Decker', 'Eddie Strong', 'Goldie Gentry', 'Jacklyn Lucas', 'Jordan Sampson', 'Linda Chapman', 'Marilyn Mcintosh', 'Naomi Buckner', 'Padilla Garrison', 'Sharron Pace', 'Solomon Fokes']`
 Виклик функції з випадковими, але валідними аргументами, повертає правильне значення */
 
+console.log('');
+console.log('tsk 4.48');
+
+const testUsers48 = [
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        eyeColor: "blue",
+        friends: ["Sharron Pace"],
+        isActive: false,
+        balance: 2811,
+        gender: "male"
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        eyeColor: "blue",
+        friends: ["Briana Decker", "Sharron Pace"],
+        isActive: true,
+        balance: 3821,
+        gender: "female"
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        eyeColor: "green",
+        friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+        isActive: false,
+        balance: 3793,
+        gender: "male"
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        eyeColor: "green",
+        friends: ["Goldie Gentry", "Aisha Tran"],
+        isActive: true,
+        balance: 2278,
+        gender: "female"
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        eyeColor: "blue",
+        friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+        isActive: true,
+        balance: 3951,
+        gender: "male"
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        eyeColor: "brown",
+        friends: ["Jacklyn Lucas", "Linda Chapman", "Adrian Cross", "Solomon Fokes"],
+        isActive: false,
+        balance: 1498,
+        gender: "male"
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        eyeColor: "brown",
+        friends: ["Goldie Gentry", "Briana Decker"],
+        isActive: true,
+        balance: 2764,
+        gender: "female"
+    }
+];
+
+/*
+Доповни функцію getTotalBalanceByGender(users, gender) таким чином, щоб вона повертала загальний 
+баланс користувачів (властивість balance), стать яких (властивість gender) збігається зі значенням параметра gender.
+
+    Оголошена змінна getTotalBalanceByGender
+    Змінній getTotalBalanceByGender присвоєна стрілочна функція з параметрами (users, gender)
+    У тілі функції використовується ланцюжок методів в правильному порядку
+    Значення параметра users не змінюється
+    Якщо значення параметра gender - це рядок "male", функція повертає число 12053
+    Якщо значення параметра gender - це рядок "female", функція повертає число 8863
+    Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+
+*/
+/*
+const calculateTotalBalance = users => {
+    return users.reduce((summ, user) => {
+        return (summ + user.balance);
+    }, 0);
+}
+*/
+// Change code below this line
+const getTotalBalanceByGender = (users, gender) => {
+    return [...users].filter(usersCopy => usersCopy.gender === gender).reduce((summBalance, user) => {
+        return (summBalance +
+            user.balance);
+    }, 0);
+};
+// Change code above this line
+
+console.log(getTotalBalanceByGender(testUsers48, "male"));
+console.log(getTotalBalanceByGender(testUsers48, "female"));
+
+//12053
+//8863
+
+/*
+Оголошена змінна `getTotalBalanceByGender`
+Змінній `getTotalBalanceByGender` присвоєна стрілочна функція з параметрами `(users, gender)`
+У тілі функції використовується ланцюжок методів в правильному порядку
+Значення параметра `users` не змінюється
+Якщо значення параметра `gender` - це рядок `'male'`, функція повертає число `12053`
+Якщо значення параметра `gender` - це рядок `'female'`, функція повертає число `8863`
+Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
+*/
+
