23090d3ccc8561a295da0771f97401cc71a7bf99
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-19 01:15:14 +0200

    tsk 5.11 started

--- js/Test-lesson-05.js
+++ js/Test-lesson-05.js
@@ -734,7 +734,7 @@ console.log('tsk 5.10');
 
 class Storage {
     constructor([...args]) { 
-        this.items = [args];
+        this.items = [...args];
     }
     getItems() {
         return this.items
@@ -743,7 +743,7 @@ class Storage {
         this.items.push(newItem); 
     }
     removeItem(itemToRemove) { 
-        this.items.pop();
+        this.items.splice(this.items.indexOf(itemToRemove),1);
     }
 }
 
@@ -755,3 +755,69 @@ storage.addItem("Droid");
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem("Prolonger");
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+/*
+ Оголошений клас `Storage`
+В класі `Storage` оголошений метод `getItems`
+В класі `Storage` оголошений метод `addItem`
+В класі `Storage` оголошений метод `removeItem
+Метод `getItems` повертає значення властивості `items` екземпляра класу, 
+який його викликає
+В результаті виклику `new Storage(['Nanitoids', 'Prolonger', 'Antigravitator'])` 
+значення змінної `storage` - це об'єкт
+Об'єкт `storage` містить властивість `items`
+*/
+
+
+console.log('');
+console.log('tsk 5.11');
+
+
+//Задача: конструктор рядків
+/*
+Напиши клас StringBuilder, який приймає один параметр initialValue - довільний рядок, 
+який записується у властивість value об'єкта, що створюється.
+
+Оголоси наступні методи класу:
+
+    getValue() - повертає поточне значення властивості value.
+    padEnd(str) - отримує параметр str (рядок) і додає його в кінець значення властивості value об'єкта, який викликає цей метод.
+    padStart(str) - отримує параметр str (рядок) і додає його на початок значення властивості value об'єкта, який викликає цей метод.
+    padBoth(str) - отримує параметр str (рядок) і додає його на початок і в кінець значення властивості value об'єкта, який викликає цей метод.
+
+Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності,
+ в якій твій код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+    Оголошений клас StringBuilder
+    В класі StringBuilder оголошений метод getValue
+    Метод getValue повертає значення властивості value екземпляра класу, який його викликає
+    В класі StringBuilder оголошений метод padEnd
+    Метод padEnd змінює властивість value екземпляра класу, який його викликає
+    В класі StringBuilder оголошений метод padStart
+    Метод padStart змінює властивість value екземпляра класу, який його викликає
+    В класі StringBuilder оголошений метод padBoth
+    Метод padBoth змінює властивість value екземпляра класу, який його викликає
+    В результаті виклику new StringBuilder(".") значення змінної builder - це об'єкт
+    Об'єкт builder містить властивість value
+    Перший виклик builder.getValue(), відразу після ініціалізації екземпляра, повертає рядок .
+    Другий виклик builder.getValue(), після виклику builder.padStart("^"), повертає рядок ^.
+    Третій виклик builder.getValue(), після виклику builder.padEnd("^"), повертає рядок ^.^
+    Четвертий виклик builder.getValue(), після виклику builder.padBoth("="), повертає рядок =^.^=
+
+*/
+
+/*
+
+
+
+// Change code above this line
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+*/
