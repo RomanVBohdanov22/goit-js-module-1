133f24b78181944339931947d3495985edac581e
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-19 00:55:18 +0200

    5.10 started

--- js/Test-lesson-05.js
+++ js/Test-lesson-05.js
@@ -546,3 +546,212 @@ console.log(carNissan);
 В результаті виклику `new Car( 'BMW', 'X5', 58900)` утвориться об'єкт `{brand: 'BMW', model: 'X5', price: 58900 }`
 В результаті виклику `new Car( 'Nissan', 'Murano', 31700)` утвориться об'єкт `{brand: 'Nissan', model: 'Murano', price: 31700 }`
 */
+
+
+console.log('');
+console.log('tsk 5.08');
+
+/*
+
+
+Клас може приймати велику кількість вхідних даних для властивостей майбутнього об'єкта. 
+Якщо параметрів багато (більше 2-х), то, як правило, застосовують патерн «Об'єкт параметрів». 
+Ідея цього патерну у тому, щоб передавати як параметр один об'єкт з логічно іменованими властивостями.
+ Значення властивостей такого об'єкта замінять набір аргументів.
+
+class User {
+  // Деструктуризуємо об'єкт
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+const mango = new User({
+  name: "Mango",
+  email: "mango@mail.com",
+});
+console.log(mango); // { name: "Mango", email: "mango@mail.com" }
+
+const poly = new User({
+  name: "Poly",
+  email: "poly@mail.com",
+});
+console.log(poly); // { name: "Poly", email: "poly@mail.com" }
+
+
+*/
+/*
+Виконай рефакторинг класу Car таким чином, щоб він приймав один параметр - 
+об'єкт з властивостями brand, model і price. Деструктуризуй об'єкт в сигнатурі (підписі) конструктора.
+
+    Оголошений клас Car
+    Клас Car має метод constructor
+    В результаті виклику new Car({ brand: "Audi", model: "Q3", price: 36000 }) 
+    утвориться об'єкт { brand: "Audi", model: "Q3", price: 36000 }
+    В результаті виклику new Car({ brand: "BMW", model: "X5", price: 58900 }) 
+    утвориться об'єкт { brand: "BMW", model: "X5", price: 58900 }
+    В результаті виклику new Car({ brand: "Nissan", model: "Murano", price: 31700 }) 
+    утвориться об'єкт { brand: "Nissan", model: "Murano", price: 31700 }
+
+
+*/
+
+class Car08 {
+  // Change code below this line
+    constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code above this line
+}
+
+const hereAudy = new Car({ brand: "Audi", model: "Q3", price: 36000 });
+const hereBMW = new Car({ brand: "BMW", model: "X5", price: 58900 });
+const hereNissan = new Car({ brand: "Nissan", model: "Murano", price: 31700 }) ;
+
+console.log(hereAudy);
+console.log(hereBMW);
+console.log(hereNissan);
+
+/*
+ Оголошений клас `Car`
+Клас `Car` має метод `constructor`
+В результаті виклику `new Car({ brand: 'Audi', model: 'Q3', price: 36000 })` утвориться об'єкт `{ brand: 'Audi', model: 'Q3', price: 36000 }`
+В результаті виклику `new Car({ brand: 'BMW', model: 'X5', price: 58900 })` утвориться об'єкт `{brand: 'BMW', model: 'X5', price: 58900 }`
+В результаті виклику `new Car({ brand: 'Nissan', model: 'Murano', price: 31700 })` утвориться об'єкт `{brand: 'Nissan', model: 'Murano', price: 31700 }`
+*/
+
+console.log('');
+console.log('tsk 5.09');
+/*
+
+
+Для роботи з властивостями майбутнього екземпляра використовуються методи класу. 
+Методи - це просто функції, але з однією відмінністю - вони доступні екземпляру класу.
+
+class User {
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
+
+  // Метод getEmail
+  getEmail() {
+    return this.email;
+  }
+
+  // Метод changeEmail
+  changeEmail(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+
+*/
+/*
+Додай класу Car два методи.
+
+    getPrice() - повертає значення властивості price з об'єкта, який буде його викликати.
+    changePrice(newPrice) - оновлює значення властивості price в об'єкта, який буде його викликати на newPrice.
+
+    В класі Car оголошений метод getPrice
+    Метод getPrice повертає значення властивості price екземпляра класу, який його викликає
+    В класі Car оголошений метод changePrice
+    Метод changePrice змінює значення властивості price екземпляра класу, який його викликає
+
+*/
+
+class Car09 {
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+  }
+  // Change code below this line
+    getPrice() { 
+        return this.price;
+    }
+    changePrice(newPrice) { 
+        this.price = newPrice;
+    }
+
+  // Change code above this line
+}
+
+/*
+Оголошений клас `Car`
+Конструктор класу приймає об'єкт з властивостями `brand`, `model` і `price`
+Виклик `Car.prototype.hasOwnProperty('getPrice')` повертає `true`
+Значення `Car.prototype.getPrice`- це функція
+Виклик `Car.prototype.hasOwnProperty('changePrice')` повертає `true`
+Значення `Car.prototype.changePrice`- це функція
+В об'єкта, створеного викликом `new Car({ brand: 'Audi', model: 'Q3', price: 36000 })`, 
+виклик метода `getPrice()` поверне число `36000`
+В об'єкта, створеного викликом `new Car({ brand: 'Audi', model: 'Q3', price: 36000 })`, 
+виклик метода `changePrice(35000)` і за наступного виклику `getPrice()` поверне число `35000`
+*/
+
+
+//------------------------------------------
+
+console.log('');
+console.log('tsk 5.10');
+//Задача: склад
+/*
+Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. Клас очікує тільки 
+один аргумент - початковий масив товарів, який записується у властивість items об'єкта, що створюється.
+
+Оголоси наступні методи класу:
+
+    getItems() - повертає масив поточних товарів у властивості items об'єкта, який викликає цей метод.
+    addItem(newItem) - приймає новий товар newItem і додає його в масив товарів у властивості items об'єкта,
+     який викликає цей метод.
+    removeItem(itemToRemove) - приймає товар itemToRemove і видаляє його з масиву товарів 
+    у властивості items об'єкта, який викликає цей метод.
+
+Під коментарем ми додали ініціалізацію екземпляра і виклики методів у тій послідовності, в якій твій 
+код перевірятимуть тести. Будь ласка, нічого там не змінюй.
+
+    Оголошений клас Storage
+    В класі Storage оголошений метод getItems
+    В класі Storage оголошений метод addItem
+    В класі Storage оголошений метод removeItem
+    Метод getItems повертає значення властивості items екземпляра класу, який його викликає
+    Метод addItem змінює властивість items екземпляра класу, який його викликає
+    Метод removeItem змінює властивість items екземпляра класу, який його викликає
+    В результаті виклику new Storage(["Nanitoids", "Prolonger", "Antigravitator"]) значення змінної storage - це об'єкт
+    Об'єкт storage містить властивість items
+    Перший виклик storage.getItems(), відразу після інціалізаціі екземпляра, 
+    повертає масив ["Nanitoids", "Prolonger", "Antigravitator"]
+    Другий виклик, storage.getItems(), після виклику storage.addItem("Droid"), 
+    повертає масив ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+    Третій виклик storage.getItems(), після виклику storage.removeItem("Prolonger"), 
+    повертає масив ["Nanitoids", "Antigravitator", "Droid"]
+
+*/
+
+class Storage {
+    constructor([...args]) { 
+        this.items = [args];
+    }
+    getItems() {
+        return this.items
+    }
+    addItem(newItem) { 
+        this.items.push(newItem); 
+    }
+    removeItem(itemToRemove) { 
+        this.items.pop();
+    }
+}
+
+
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
