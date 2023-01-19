6ec496e94a027da3af758fae100a7c9ff0d97d8d
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-19 17:39:04 +0200

    tsk 5-20 started

--- js/Test-lesson-05.js
+++ js/Test-lesson-05.js
@@ -1436,4 +1436,216 @@ console.log(Car17.checkPrice(bmw17.price)); // "Error! Price exceeds the maximum
 */
 
 
+console.log('');
+console.log('tsk 5.18');
+/*
 
+
+Ключове слово extends дозволяє реалізувати наслідування класів, коли один клас (дочірній, похідний) 
+наслідує властивості та методи іншого класу (батьківського).
+
+class Child extends Parent {
+  // ...
+}
+
+У виразі class Child extends Parent дочірній клас Child наслідує (розширює) від батьківського класу Parent. 
+Це означає, що ми можемо оголосити базовий клас, який зберігає загальні характеристики 
+і методи для групи похідних класів, які наслідують властивості і методи батьківського, але також додають свої унікальні.
+
+Наприклад, у застосунку є користувачі з різними ролями — адміністратор, копірайтер, контент менеджер тощо. 
+У кожного типу користувача є набір загальних характеристик, наприклад, пошта і пароль, але також є й унікальні.
+
+Створивши незалежні класи для кожного типу користувача, ми отримаємо дублювання загальних властивостей і методів, 
+і, якщо необхідно змінити, наприклад, назву властивості, доведеться проходити по усіх класах, а це незручно і вимагає багато часу.
+
+Замість цього, можна створити загальний клас User, який буде зберігати набір загальних властивостей і методів, 
+після чого, створити класи для кожного типу користувача, які наслідують цей набір від класу User. За потреби змінити 
+щось спільне, достатньо буде змінити тільки код класу User.
+
+class User {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class ContentEditor extends User {
+  // Тіло класу ContentEditor
+}
+
+const editor = new ContentEditor("mango@mail.com");
+console.log(editor); // { email: "mango@mail.com" }
+console.log(editor.email); // "mango@mail.com"
+
+Клас ContentEditor наслідує від класу User його конструктор, гетер і сетер email, 
+а також однойменну публічну властивість. Важливо пам'ятати, що приватні властивості і методи
+ батьківського класу не наслідуються дочірнім класом.
+
+*/
+
+/*
+У застосунку потрібен адміністратор з можливістю додавати пошти користувачів у чорний список.
+
+    Оголоси клас Admin, який наслідує від класу User
+    Додай класу Admin публічну статичну властивість AccessLevel (рівень доступу), значення якої — 
+    це об'єкт {BASIC: "basic", SUPERUSER: "superuser"}
+
+    Оголошений клас Admin
+    Клас Admin наслідує від класу User
+    Клас Admin містить публічну статичну властивість AccessLevel
+    Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+    Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
+
+
+*/
+
+
+class User {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+// Change code below this line
+
+class Admin extends User { 
+    static AccessLevel = { BASIC: "basic", SUPERUSER: "superuser" };
+}
+
+console.log(Admin.AccessLevel.BASIC);
+console.log(Admin.AccessLevel.SUPERUSER);
+
+/*
+Оголошений клас `Admin`
+Клас `Admin` наслідує від класу `User`
+Клас `Admin` містить публічну статичну властивість `AccessLevel`
+Звернення до 'Admin.AccessLevel.BASIC' повертає рядок 'basic'
+Звернення до 'Admin.AccessLevel.SUPERUSER' повертає рядок 'superuser'
+*/
+
+console.log('');
+console.log('tsk 5.19');
+
+/*
+
+
+Насамперед в конструкторі дочірнього класу необхідно викликати спеціальну функцію super(аргументи) 
+- це псевдонім конструктора батьківського класу. В іншому випадку, при спробі звернутися до this в 
+конструкторі дочірнього класу, виникне помилка. Під час виклику конструктора батьківського класу 
+передаємо необхідні йому аргументи для ініціалізації властивостей.
+
+class User {
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class ContentEditor extends User {
+  constructor({ email, posts }) {
+    // Виклик конструктора батьківського класу User
+    super(email);
+    this.posts = posts;
+  }
+}
+
+const editor = new ContentEditor({ email: "mango@mail.com", posts: [] });
+console.log(editor); // { email: 'mango@mail.com', posts: [] }
+console.log(editor.email); // 'mango@mail.com'
+
+
+*/
+
+/*
+Додай класу Admin метод constructor, який приймає один параметр - об'єкт налаштувань з 
+двома властивостями email і accessLevel. Додай класу Admin публічну властивість accessLevel, 
+значення якої буде передаватися під час виклику конструктора.
+
+Щоб показати, як буде використовуватися клас Admin, ми додали ініціалізацію екземпляра під оголошенням класу.
+
+    Оголошений клас Admin
+    Клас Admin наслідує від класу User
+    Клас Admin містить публічну статичну властивість AccessLevel
+    Клас Admin містить метод constructor з параметром у вигляді об'єкта {email, accessLevel}
+    В класі Admin в конструкторі для властивості email використовується звернення до конструктора батьківського класу
+    Звернення до Admin.AccessLevel.BASIC повертає рядок "basic"
+    Звернення до Admin.AccessLevel.SUPERUSER повертає рядок "superuser"
+
+
+*/
+
+class User19 {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin19 extends User19 {
+  // Change code below this line
+
+  static AccessLevel = {
+    BASIC: "basic",
+    SUPERUSER: "superuser",
+  };
+    constructor({email, accessLevel}) { 
+        super(email);
+        this.accessLevel = accessLevel;
+    }
+  // Change code above this line
+}
+
+const mango = new Admin19({
+  email: "mango@mail.com",
+  accessLevel: Admin19.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+/*
+mango@mail.com
+superuser
+*/
+/*
+Оголошений клас `Admin`
+Клас `Admin` наслідує від класу `User`
+Клас `Admin` містить публічну статичну властивість `AccessLevel`
+Клас `Admin` містить метод `constructor` з параметром у вигляді об'єкта `{email, accessLevel}`
+В класі `Admin` в конструкторі для властивості `email` використовується 
+звернення до конструктора батьківського класу
+Звернення до 'Admin.AccessLevel.BASIC' повертає рядок 'basic'
+Звернення до 'Admin.AccessLevel.SUPERUSER' повертає рядок 'superuser'
+*/
+
+console.log('');
+console.log('tsk 5.20');
