4a7ee3802e418a64e79e00702248468ac5793479
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-20 22:45:57 +0200

    Властивості і атрибути

--- js/for-less-6-test-001.js
+++ js/for-less-6-test-001.js
@@ -1,3 +1,5 @@
+// ---- Навігація по DOM
+
 console.log('');
 
 console.log(document);
@@ -13,3 +15,33 @@ console.log(firstListItem);
 
 const listItems = list.children;
 console.log(listItems);
+
+//---------- Пошук елементів
+console.log('');
+
+const listWithId = document.querySelector('#menu');
+listWithId.style.textTransform = 'uppercase';
+listWithId.style.fontSize = '24px';
+console.log(listWithId);
+
+const listWithClass = document.querySelector('.menu');
+console.log(listWithClass);
+
+const menuItemsByTagName = document.querySelectorAll("li");
+console.log(menuItemsByTagName);
+
+const menuItemsByClass = document.querySelectorAll(".menu-item");
+console.log(menuItemsByClass);
+
+const firstMenuItem = document.querySelector(".menu-item");
+firstMenuItem.style.color = 'tomato';
+console.log(firstMenuItem);
+
+// ---- Властивості та атрибути
+
+/*
+    value - містить поточний текстовий контент елементів форм.
+    checked - зберігає стан чекбокса або радіокнопки.
+    name - зберігає значення, вказане в HTML-атрибуті name.
+    src - шлях до зображення тегу <img>.
+*/
