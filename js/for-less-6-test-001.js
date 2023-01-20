7b9b7d3f41462c7bfdabf64e333c96ad453cf887
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-20 22:32:01 +0200

    DOM started

---
+++ js/for-less-6-test-001.js
@@ -0,0 +1,15 @@
+console.log('');
+
+console.log(document);
+
+const body = document.body;
+console.log(body);
+
+const list = body.firstElementChild;
+console.log(list);
+
+const firstListItem = list.firstElementChild;
+console.log(firstListItem);
+
+const listItems = list.children;
+console.log(listItems);
