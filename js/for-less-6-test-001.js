04ebe5ec7325dbf9e4dc11a54306507f74f5b4f3
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-01-21 22:41:11 +0200

    marazm

--- js/for-less-6-test-001.js
+++ js/for-less-6-test-001.js
@@ -45,3 +45,15 @@ console.log(firstMenuItem);
     name - зберігає значення, вказане в HTML-атрибуті name.
     src - шлях до зображення тегу <img>.
 */
+
+const message = document.querySelector("#message");
+console.log(message.value); // Default textarea message
+
+//===================================================
+const activeLink = document.querySelector(".btn.active");
+console.log(activeLink.href); // https://s.codepen.io/about
+
+//===================================================
+const image = document.querySelector(".image");
+console.log(image.src); // https://placeimg.com/640/480/animals
+image.src = 'https://placeimg.com/640/480/tech';
